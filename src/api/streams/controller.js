23b78ec73d1ea865acbf836b67721ab0fdab44e7
A.Kasavets <a.kasavets@hqsoftwarelab.com>
2020-05-06 17:33:43 +0300

    use simple proxy

--- src/api/streams/controller.js
+++ src/api/streams/controller.js
@@ -5,7 +5,7 @@ module.exports = {
   allStreams: (request) => {
     const { info } = request;
     console.log(JSON.stringify(info));
-    const camResources = safeLoad(fs.readFileSync(`${__dirname}/../../../.cam-resources.yml`, 'utf8'));
+    const camResources = safeLoad(fs.readFileSync(`${__dirname}/../../../cam-resources.yml`, 'utf8'));
     const { rtspPort } = camResources.server;
     return Object.keys(camResources.streams).map((streamAlias) => `rtsp://${info.remoteAddress}:${rtspPort}/${streamAlias}`);
   },
