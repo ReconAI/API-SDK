9f9093feb925958fa7c681f48454b81ec4b7642c
A.Kasavets <a.kasavets@hqsoftwarelab.com>
2020-05-06 18:13:10 +0300

    remote address to host

--- src/api/streams/controller.js
+++ src/api/streams/controller.js
@@ -3,11 +3,12 @@ const { safeLoad } = require('js-yaml');
 
 module.exports = {
   allStreams: (request) => {
-    const { info } = request;
-    console.log(JSON.stringify(info));
+    const { host } = request.headers;
+    // console.log(JSON.stringify(host));
+    const base = host.split(':')[0];
     const camResources = safeLoad(fs.readFileSync(`${__dirname}/../../../cam-resources.yml`, 'utf8'));
     const { rtspPort } = camResources.server;
-    return Object.keys(camResources.streams).map((streamAlias) => `rtsp://${info.remoteAddress}:${rtspPort}/${streamAlias}`);
+    return Object.keys(camResources.streams).map((streamAlias) => `rtsp://${base}:${rtspPort}/${streamAlias}`);
   },
   getStream: (request, h) => {
     const { logger } = request.server;
